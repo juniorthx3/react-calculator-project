63d231b56b1ee893fd8d87a7263e582d385b90c6
Birkhoff <juniorthx@gmail.com>
2020-02-23 14:09:05 -0800

    Updates

---
+++ precache-manifest.a172e93ff7d5232b6a3e3ffc196313d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7aef55133edfefeb8d7344a293fbc3",
+    "url": "/react-calculator-project/index.html"
+  },
+  {
+    "revision": "3214e68ecca02c0f8c7f",
+    "url": "/react-calculator-project/static/css/main.3d9fbf99.chunk.css"
+  },
+  {
+    "revision": "513e0b7d64e0488990ad",
+    "url": "/react-calculator-project/static/js/2.c11a9e70.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-calculator-project/static/js/2.c11a9e70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3214e68ecca02c0f8c7f",
+    "url": "/react-calculator-project/static/js/main.cefb686b.chunk.js"
+  },
+  {
+    "revision": "bec9780b6d7d7554e846",
+    "url": "/react-calculator-project/static/js/runtime-main.c5e9271e.js"
+  }
+]);
